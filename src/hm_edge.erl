90c38e3eb1d2b3964f8cd46d87a594e71e1e11fb
Yoshihiro TANAKA <hirotnkg@gmail.com>
2010-09-04 08:30:48 -0700

    added application stop mechanism
    modified file name from hm_startup.erl -> hm_edge.erl

---
+++ src/hm_edge.erl
@@ -0,0 +1,27 @@
+-module(hm_edge).
+-export([
+        start/0,
+        stop/0
+        ]).
+
+start() ->
+    case application:start(harmonia) of
+        ok -> 
+            io:format("harmonia application started.~n");
+        {error, Msg} ->
+            io:format("harmonia application start failed:~p~n", Msg)
+    end.
+
+% Note: This API needs to be executed on the node on which hm_name_server runs
+%       because it has to be already connected to all nodes.
+%
+% this API takes a while to finish.
+stop() -> 
+    {ok, NameList} = gen_server:call({global, hm_name_server}, get_name_list),
+    stop_in(NameList).
+stop_in([]) -> ok;
+stop_in([Name|NameList]) ->
+    {ok, {sname, NodeName}} = gen_server:call({global, hm_config_if:name(Name)}, {get, sname}),
+    ok = rpc:call(NodeName, application, stop, [harmonia]),
+    ok = rpc:call(NodeName, init, stop, []),
+    stop_in(NameList).
